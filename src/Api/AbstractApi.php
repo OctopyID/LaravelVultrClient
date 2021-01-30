a885892a6d14f6a0c4288eed8ce1fff39bfec6bf
SupianIDz <supianidz@gmail.com>
2021-01-31 03:12:14 +0800

    Add Relationship

--- src/Api/AbstractApi.php
+++ src/Api/AbstractApi.php
@@ -2,15 +2,78 @@
 
 namespace Octopy\Vultr\Api;
 
+use Closure;
+use Octopy\Vultr\Relation;
+use Octopy\Vultr\Handler\AbstractHandler;
 use Octopy\Vultr\Adapter\AdapterInterface;
 
 abstract class AbstractApi
 {
+	protected array $relations = [];
+
 	/**
 	 * AbstractApi constructor.
 	 * @param  AdapterInterface $adapter
 	 */
 	public function __construct(protected AdapterInterface $adapter)
 	{
+	}
+
+	/**
+	 * @param  string|array $relations
+	 * @return AbstractApi
+	 */
+	public function with(string|array $relations) : AbstractApi
+	{
+		$relations = is_string($relations) ? func_get_args() : $relations;
+
+		foreach ($relations as $relation => $callback) {
+			$this->parseRelation($relation, $callback);
+		}
+
+		return $this;
+	}
+
+	/**
+	 * @param  AbstractHandler $handler
+	 * @return AbstractHandler
+	 */
+	protected function handle(AbstractHandler $handler) : AbstractHandler
+	{
+		foreach ($this->relations as $builder) {
+			$handler = $handler->map(function (AbstractHandler $handler) use ($builder) {
+				return $builder->handle($handler, $this);
+			});
+		}
+
+		return $handler;
+	}
+
+	/**
+	 * @param  string              $relation
+	 * @param  Closure|string|null $callback
+	 * @return Relation
+	 */
+	private function parseRelation(string $relation, Closure|string|null $callback) : Relation
+	{
+		if (is_numeric($relation)) {
+			$relation = $callback;
+		}
+
+		if (preg_match('/:/', $relation)) {
+			[$method, $primary] = array_map('trim', explode(':', $relation, 2));
+		} else {
+			return $this->parseRelation($relation . ':' . 'id', $callback);
+		}
+
+		if (preg_match('/ AS /', $method ?? '')) {
+			[$method, $alias] = array_map('trim', explode(' AS ', $method));
+		} else if (preg_match('/ AS /', $primary ?? '')) {
+			[$primary, $alias] = array_map('trim', explode(' AS ', $primary));
+		}
+
+		return $this->relations[] = new Relation($method, $alias ?? null, $primary, $callback instanceof Closure ? $callback : function ($data) {
+			return $data;
+		});
 	}
 }
---
+++ src/Relation.php
@@ -0,0 +1,118 @@
+<?php
+
+namespace Octopy\Vultr;
+
+use Closure;
+use TypeError;
+use ReflectionMethod;
+use ReflectionException;
+use ReflectionParameter;
+use Octopy\Vultr\Api\AbstractApi;
+use Octopy\Vultr\Handler\AbstractHandler;
+
+final class Relation
+{
+	/**
+	 * @var Closure
+	 */
+	private Closure $result;
+
+	/**
+	 * @var array
+	 */
+	private array $params = [];
+
+	/**
+	 * Builder constructor.
+	 * @param  string      $method
+	 * @param  string|null $alias
+	 * @param  string      $primary
+	 * @param  Closure     $callback
+	 */
+	public function __construct(protected string $method, protected string|null $alias, protected string $primary, Closure $callback)
+	{
+		$this->result = function ($result) {
+			return $result;
+		};
+
+		$callback($this);
+	}
+
+	/**
+	 * @param  string $alias
+	 */
+	public function alias(string $alias)
+	{
+		$this->alias = $alias;
+	}
+
+	/**
+	 * @param  array $where
+	 */
+	public function where(array $where)
+	{
+		$this->params = array_merge($this->params, $where);
+	}
+
+	/**
+	 * @param  Closure $handler
+	 */
+	public function result(Closure $handler)
+	{
+		$this->result = $handler;
+	}
+
+	/**
+	 * @param  AbstractHandler $handler
+	 * @param  AbstractApi     $api
+	 * @return AbstractHandler
+	 * @throws ReflectionException
+	 */
+	public function handle(AbstractHandler $handler, AbstractApi $api) : AbstractHandler
+	{
+		$result = $this->result;
+		$this->where([
+			$this->primary => $handler->{$this->primary},
+		]);
+
+		return $handler->put(($this->alias ?? $this->method), $result(
+			$api->{$this->method}(...$this->getParameters($api))
+		));
+	}
+
+	/**
+	 * @param  AbstractApi $api
+	 * @return array
+	 * @throws TypeError
+	 * @throws ReflectionException
+	 */
+	private function getParameters(AbstractApi $api) : array
+	{
+		$reflection = new ReflectionMethod($api, $this->method);
+
+		return collect($reflection->getParameters())->map(function (ReflectionParameter $dependency) {
+			$name = $dependency->getName();
+
+			if (isset($this->params[$name])) {
+				return $this->params[$name];
+			}
+
+			if ($dependency->isDefaultValueAvailable()) {
+				return $dependency->getDefaultValue();
+			}
+
+			$this->throwMissingRequiredArgs($dependency);
+		})->toArray();
+	}
+
+	/**
+	 * @param  ReflectionParameter $reflection
+	 * @throws TypeError
+	 */
+	private function throwMissingRequiredArgs(ReflectionParameter $reflection,)
+	{
+		throw new TypeError(
+			sprintf("%s() : Argument #%s (%s $%s) is missing", $this->method, $reflection->getPosition(), $reflection->getType(), $reflection->getName())
+		);
+	}
+}
